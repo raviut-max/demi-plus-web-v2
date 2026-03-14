208899a324236bad2384335c224ee70cf6d947aa
Ratsapan Phuksaritanon <ratsapanphuksaritanon@Ratsapans-MacBook-Air.local>
2026-03-14 16:11:11 +0700

    fix postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-  // ✅ ปิด TypeScript checking ระหว่าง build
   typescript: {
     ignoreBuildErrors: true,
   },
-  // ✅ รองรับ images จาก Supabase
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**.supabase.co',
+        protocol: "https",
+        hostname: "**.supabase.co",
       },
     ],
   },
