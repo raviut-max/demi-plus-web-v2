b4e2b920b0b89e765ad3374f92ae1fbcd220e591
Ratsapan Phuksaritanon <ratsapanphuksaritanon@Ratsapans-MacBook-Air.local>
2026-03-14 16:13:26 +0700

    fix postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,17 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.supabase.co",
-      },
-    ],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
-
-export default nextConfig;
